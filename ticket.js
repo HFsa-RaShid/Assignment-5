f9b36bbd8bdb59a6ac37a0db2980cd5a341312e6
HFsa-RaShid <hafsa.cse7.bu@gmail.com>
2024-02-21 00:50:12 +0600

    seats selected

---
+++ ticket.js
@@ -0,0 +1,31 @@
+function scrollToTicketSection(){
+    document.getElementById('scrollToTicketSection').scrollIntoView({behavior: 'smooth'});
+}
+let count=0;
+let availavleSeats=40;
+
+function changeSeatBackground(element){
+    
+    if(count<2){
+        element.style.backgroundColor= '#1DD100';
+        count++;
+        availavleSeats--;
+        document.getElementById('availavleSeat').innerText=availavleSeats;
+
+    } 
+    else
+    {
+        openModal();
+    }
+
+}
+function openModal()
+{
+    document.getElementById('my_modal_5').showModal();
+
+}
+function closeModal()
+{
+    document.getElementById('my_modal_5').close();
+
+}
