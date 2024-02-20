424bcdfe63d9d6fe6c05daf74725035ee5f4a428
HFsa-RaShid <hafsa.cse7.bu@gmail.com>
2024-02-21 02:27:30 +0600

    dynamic seat selection

--- ticket.js
+++ ticket.js
@@ -2,17 +2,28 @@ function scrollToTicketSection(){
     document.getElementById('scrollToTicketSection').scrollIntoView({behavior: 'smooth'});
 }
 let count=0;
-let availavleSeats=40;
+let availableSeats=40;
 
 function changeSeatBackground(element){
+    let seat=document.getElementById(element);
     
-    if(count<4){
-        element.style.backgroundColor= '#1DD100';
+    if(count<4 && !seat.classList.contains('selected')){
+        seat.style.backgroundColor= '#1DD100';
         count++;
-        availavleSeats--;
-        document.getElementById('availavleSeat').innerText=availavleSeats;
+        availableSeats--;
+        document.getElementById('availableSeat').innerText=availableSeats;
+        seat.classList.toggle('selected');
 
     } 
+    else if(count<5 && seat.classList.contains('selected'))
+    {
+        seat.style.backgroundColor= '#F7F8F8';
+        count--;
+        availableSeats++;
+        document.getElementById('availableSeat').innerText=availableSeats;
+        seat.classList.remove('selected');
+
+    }
     else
     {
         openModal();
