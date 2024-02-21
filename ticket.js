cbba97532cd7fe4569e96873165b3ba5d5f26d3d
HFsa-RaShid <hafsa.cse7.bu@gmail.com>
2024-02-21 20:14:07 +0600

    append done

--- ticket.js
+++ ticket.js
@@ -3,16 +3,42 @@ function scrollToTicketSection(){
 }
 let count=0;
 let availableSeats=40;
+let countBookedSeats = 0;
 
 function changeSeatBackground(element){
     let seat=document.getElementById(element);
+    let tableInformation= document.getElementById('tableBody');
+    let newRow= tableInformation.getElementsByTagName('tr');
     
     if(count<4 && !seat.classList.contains('selected')){
         seat.style.backgroundColor= '#1DD100';
         count++;
         availableSeats--;
         document.getElementById('availableSeat').innerText=availableSeats;
-        seat.classList.toggle('selected');
+        seat.classList.add('selected');
+
+        // Append td data
+
+        
+        let row= document.createElement('tr');
+
+        let seatName= document.createElement('td');
+        let seatClass= document.createElement('td');
+        let seatPrice= document.createElement('td');
+
+        seatName.textContent = element;
+        seatClass.textContent = "Economy";
+        seatPrice.textContent = '550';
+
+        row.appendChild(seatName);
+        row.appendChild(seatClass);
+        row.appendChild(seatPrice);
+
+        tableInformation.appendChild(row);
+
+        // countBookedSeats
+        countBookedSeats++;
+        document.getElementById('countBookedSeats').innerText= countBookedSeats;
 
     } 
     else if(count<5 && seat.classList.contains('selected'))
@@ -22,6 +48,24 @@ function changeSeatBackground(element){
         availableSeats++;
         document.getElementById('availableSeat').innerText=availableSeats;
         seat.classList.remove('selected');
+
+        countBookedSeats--;
+        document.getElementById('countBookedSeats').innerText= countBookedSeats;
+        
+
+        for(let i=0;i<newRow.length;i++)
+        {
+            let cell = newRow[i].getElementsByTagName('td');
+            if(cell.length>0 && cell[0].textContent===element)
+            {
+                tableInformation.removeChild(newRow[i]);
+                break;
+
+            }
+
+        }
+
+        
 
     }
     else
@@ -39,4 +83,8 @@ function closeModal()
 {
     document.getElementById('my_modal_5').close();
 
+}
+function info()
+{
+    
 }
